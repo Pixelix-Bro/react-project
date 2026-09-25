3f5149f4ff80b46c8dece6a6ea039cc6675dec28
ubaydulloh dadakxanov <lazizbekxoljigitov@gmail.com>
2026-09-25 21:57:26 +0500

    Initial commit

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://react-project-puce-eta.vercel.app/data/data.json",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
